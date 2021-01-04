e44ac2798991c4451c82cb6bc352944fd501319b
Sanjsarma <sangeethasanjana@gmail.com>
2021-01-04 16:18:13 +0530

    Added user table

--- itemstable.sql
+++ itemstable.sql
@@ -8,3 +8,10 @@ image VARCHAR(255),
 status CHAR DEFAULT "y", 
 offer CHAR DEFAULT "n", 
 PRIMARY KEY(i_no))ENGINE=InnoDB;
+
+create table user(id INTEGER AUTO_INCREMENT,
+name VARCHAR(50) not null, 
+email VARCHAR(50) NOT NULL, 
+password VARCHAR(200) NOT NULL, 
+role VARCHAR(50) default "customer", 
+primary key(id))ENGINE=InnoDB;
